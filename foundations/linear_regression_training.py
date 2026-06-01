c375359b3a0d3b5698ff2d7ba71fdd169c037e1f
Yarikama <125861728+yarikama@users.noreply.github.com>
2026-05-31 19:47:58 -0500

    Implement Linear regression training (Math Foundations 6/6)

---
+++ foundations/linear_regression_training.py
@@ -0,0 +1,47 @@
+import numpy as np
+from numpy.typing import NDArray
+
+
+class Solution:
+    def get_derivative(
+        self, 
+        model_prediction: NDArray[np.float64], 
+        ground_truth: NDArray[np.float64], 
+        N: int, 
+        X: NDArray[np.float64], 
+        desired_weight: int
+    ) -> float:
+        # note that N is just len(X)
+        return -2 * np.dot(ground_truth - model_prediction, X[:, desired_weight]) / N
+
+    def get_model_prediction(
+        self, 
+        X: NDArray[np.float64], 
+        weights: NDArray[np.float64]
+    ) -> NDArray[np.float64]:
+        return np.squeeze(X @ weights)
+
+    learning_rate = 0.01
+
+    def train_model(
+        self,
+        X: NDArray[np.float64],
+        Y: NDArray[np.float64],
+        num_iterations: int,
+        initial_weights: NDArray[np.float64]
+    ) -> NDArray[np.float64]:
+
+        W = initial_weights.copy()
+    
+        # For each iteration:
+        for _ in range(num_iterations):
+    
+        #   1. Compute predictions with get_model_prediction(X, weights)
+            preds = self.get_model_prediction(X, W)
+    
+        #   2. For each weight index j, compute gradient with get_derivative()
+            for j in range(len(W)):
+                grads = self.get_derivative(preds, Y, len(X), X, j)
+            #   3. Update: weights[j] -= learning_rate * gradient
+                W[j] -= self.learning_rate * grads
+        return np.round(W, 5)
